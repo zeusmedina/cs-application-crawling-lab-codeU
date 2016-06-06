44a6de068e6a7d5cea4c0b3823fb29b231c910c4
Charles Pletcher <cpletcher2@gmail.com>
2016-06-06 11:30:58 -0400

    Update WikiCrawlerTest and local Java wiki page

--- javacs-lab11/src/com/flatironschool/javacs/WikiCrawlerTest.java
+++ javacs-lab11/src/com/flatironschool/javacs/WikiCrawlerTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.flatironschool.javacs;
 
@@ -33,13 +33,13 @@ public class WikiCrawlerTest {
 	public void setUp() throws Exception {
 		// make a WikiCrawler
 		jedis = JedisMaker.make();
-		index = new JedisIndex(jedis); 
+		index = new JedisIndex(jedis);
 		String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		wc = new WikiCrawler(source, index);
 
 		// for testing purposes, load up the queue
 		WikiFetcher wf = new WikiFetcher();
-		Elements paragraphs = wf.fetchWikipedia(source);
+		Elements paragraphs = wf.readWikipedia(source);
 		wc.queueInternalLinks(paragraphs);
 	}
 
@@ -53,25 +53,25 @@ public class WikiCrawlerTest {
 
 	/**
 	 * Test method for {@link com.flatironschool.javacs.WikiCrawler#crawl()}.
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	@Test
 	public void testCrawl() throws IOException {
 		String url1 = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		String url2 = "https://en.wikipedia.org/wiki/Programming_language";
 		String url3 = "https://en.wikipedia.org/wiki/Concurrent_computing";
-		
+
 		String res = wc.crawl(true);
 		assertThat(url1.equals(res), is(true));
-		assertThat(wc.queueSize(), is(398));
+		assertThat(wc.queueSize(), is(396));
 
 		res = wc.crawl(true);
 		assertThat(url2.equals(res), is(true));
-		assertThat(wc.queueSize(), is(655));
+		assertThat(wc.queueSize(), is(653));
 
 		res = wc.crawl(true);
 		assertThat(url3.equals(res), is(true));
-		assertThat(wc.queueSize(), is(706));
+		assertThat(wc.queueSize(), is(704));
 
 		Map<String, Integer> map = index.getCounts("the");
 
